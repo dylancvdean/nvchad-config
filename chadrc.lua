03d3deda1bab4943566fdfb0ac65df8cf97ff017
Dylan Dean <dylancvdean@gmail.com>
2024-03-07 23:15:07 -0800

    colors and options

--- chadrc.lua
+++ chadrc.lua
@@ -2,8 +2,8 @@
 local M = {}
 
 M.ui = {
-  theme = "catppuccin",
-  theme_toggle = { "catppuccin", "github_light" },
+  theme = "github_dark",
+  theme_toggle = { "github_dark", "radium" },
   statusline = {
     separator_style = "arrow",
   },
--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
-vim.opt.relativenumber = true                   --relative numbers
+vim.opt.number = true                   --line numbers
+vim.opt.hlsearch = true                 --highlight search results
+vim.opt.ignorecase = true               --ignore case when searching
+vim.opt.smartcase = true                --unless there is a capital letter
+vim.opt.autoindent = true               --autoindent
+vim.opt.cursorline = true               --highlight current line
+vim.opt.showcmd = true                  --show command in bottom bar
+vim.opt.clipboard = "unnamedplus"       --use system clipboard
 vim.opt.wrap = false                            --don't wrap long lines
 vim.opt.incsearch = true                        --allows for tricky searches like "v* = false"
 vim.opt.shell = "/bin/zsh"                      --set shell to zsh
